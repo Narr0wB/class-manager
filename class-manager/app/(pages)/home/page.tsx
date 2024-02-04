a41660aedc1fb3f0c67fe76512aae9620a11f747
Luca <marchisioluca15@gmail.com>
2024-02-04 16:20:18 +0100

    [Refactor + Add] modified next structure, added shadcn

---
+++ class-manager/app/(pages)/home/page.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils';
+
+type HomeProps = {
+}
+
+const Home: React.FC<HomeProps> = ({ }) => {
+  return (
+    <main className="flex min-h-screen items-center p-24">
+      <div className="flex items-center justify-center w-full">
+        <h1>NarrowB bestest eu</h1>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
