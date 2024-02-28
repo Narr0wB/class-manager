12b53bb2d6632e98875c8da60a17a676cfdf698c
Luca <marchisioluca15@gmail.com>
2024-02-28 12:58:15 +0100

    [Add] Added initial structure.

---
+++ class-manager/components/custom/hour-drawer.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import { useState } from 'react';
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '../ui/drawer';
+import { Button } from '../ui/button';
+
+type HourDrawerProps = {
+  className?: string;
+  id?: string;
+  children?: React.ReactNode;
+}
+
+const data = [
+  {
+    goal: 400,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 239,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 349,
+  },
+]
+
+const HourDrawer: React.FC<HourDrawerProps> = ({ className, id, children }) => {
+  const [goal, setGoal] = useState(350);
+
+  const onClick = (adjustment: number) => {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger>Open</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default HourDrawer;
