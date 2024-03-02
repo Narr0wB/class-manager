c11af451f2493a63a8c74274c0092d7219f35f0d
Luca <marchisioluca15@gmail.com>
2024-03-02 10:57:35 +0100

    [Update] Added logic to change hours. Needs fix

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,15 +1,19 @@
+"use client"
+
 import DatePicker from '@/components/custom/date-picker';
 import HourDrawer from '@/components/custom/hour-drawer';
 import { Avatar } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Drawer } from '@/components/ui/drawer';
+import { RefObject, useRef } from 'react';
 
 type HomeProps = {
 }
 
 const Home: React.FC<HomeProps> = ({ }) => {
+  const timeTextRef: RefObject<HTMLParagraphElement> = useRef(null);
+
   return (
-    <main className="h-[100vh] w-[100vw] max-h-screen max-w-screen">
+    <main className="h-[100vh] w-[100vw] max-h-screen max-w-screen overflow-hidden">
       <nav className="w-screen h-[10%]">
         <div id="user" className="flex flex-row h-full w-[20%] border-solid border-2 border-black">
           <Button>
@@ -25,10 +29,10 @@ const Home: React.FC<HomeProps> = ({ }) => {
         <div id="date-time" className="w-full h-full flex flex-col border-solid border-2 border-black">
           <div id="hour-selector" className="grid justify-between w-full aspect-[3] grid-cols-[5fr_2fr] border-solid border-2 border-black">
             <div id="hour" className="flex justify-items-center items-center border-solid border-2 border-black">
-              12.36
+              <p ref={timeTextRef}>12:00</p>
             </div>
             <div id="hour-drawer" className="border-solid border-2 border-black">
-              <HourDrawer />
+              <HourDrawer timeTextRef={timeTextRef} />
             </div>
           </div>
           <div id="date-picker" className="border-solid border-2 border-black">
--- components/custom/hour-drawer.tsx
+++ components/custom/hour-drawer.tsx
@@ -1,77 +1,80 @@
 "use client"
 
-import { useState } from 'react';
+import { RefObject, useEffect, useState } from 'react';
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '../ui/drawer';
 import { Button } from '../ui/button';
+import { MinusIcon, PlusIcon } from 'lucide-react';
 
 type HourDrawerProps = {
-  className?: string;
-  id?: string;
-  children?: React.ReactNode;
+  timeTextRef: RefObject<HTMLParagraphElement>;
 }
 
-const data = [
-  {
-    goal: 400,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 278,
-  },
-  {
-    goal: 189,
-  },
-  {
-    goal: 239,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 278,
-  },
-  {
-    goal: 189,
-  },
-  {
-    goal: 349,
-  },
-]
+const HourDrawer: React.FC<HourDrawerProps> = ({ timeTextRef }) => {
+  const [minutes, setMinutes] = useState(() => {
+    const date = new Date();
+    return date.getHours() * 60 + date.getMinutes();
+  });
 
-const HourDrawer: React.FC<HourDrawerProps> = ({ className, id, children }) => {
-  const [goal, setGoal] = useState(350);
-
-  const onClick = (adjustment: number) => {
-    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
+  const changeTime = (minutes: number) => {
+    setMinutes(before => before + minutes);
   }
+
+  const minutesToTimeString = (minutes: number) => {
+    const date = new Date();
+    date.setHours(parseInt("" + (minutes / 60)));
+    date.setMinutes(minutes % 60);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+
+    return date.getHours() + ":" + date.getMinutes();
+  };
 
   return (
     <Drawer>
       <DrawerTrigger>Open</DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+      <DrawerContent className="ml-[10%] mr-[10%] flex items-center">
+        <DrawerHeader className="max-w-[60%] p-0">
+          <DrawerTitle>Ora</DrawerTitle>
+          <DrawerDescription>Seleziona l'ora della prenotazione</DrawerDescription>
         </DrawerHeader>
-        <DrawerFooter>
-          <Button>Submit</Button>
-          <DrawerClose>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
+        <DrawerFooter className="max-w-[60%]">
+          <div className="p-4 pb-0">
+            <div className="flex items-center justify-between">
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => changeTime(-10)}
+              >
+                <MinusIcon className="h-4 w-4" />
+                <span className="sr-only">Decrease</span>
+              </Button>
+              <div className="flex-1 text-center">
+                <div className="text-7xl font-bold tracking-tighter">
+                  { }
+                </div>
+              </div>
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => changeTime(10)}
+              >
+                <PlusIcon className="h-4 w-4" />
+                <span className="sr-only">Increase</span>
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center">
+            <Button onClick={() => {
+              if (timeTextRef.current) {
+                timeTextRef.current.innerText = minutesToTimeString(minutes);
+              }
+            }}>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </div>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
