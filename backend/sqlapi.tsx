46a85c3638774ba635f3ee4c705e3adc8b3707d5
narrow <iliash4@hotmail.com>
2024-03-09 22:38:36 +0100

    [Update] Created backend folder

---
+++ backend/sqlapi.tsx
@@ -0,0 +1,52 @@
+import mysql from "mysql2"
+
+export const SQL_QUERY_INSERT_PRE = 1;
+export const SQL_QUERY_FETCH_PRE = 2;
+export const SQL_QUERY_INSERT_UTENTE = 3;
+export const SQL_QUERY_FETCH_UTENTE = 4;
+export const SQL_QUERY_FETCH_AULA = 5;
+
+export const queries_strings: { [id: number] : string } = {
+    [SQL_QUERY_INSERT_PRE]: "INSERT INTO AM_Prenotazioni(id_utente, id_aula, data_ora_pre, approvazione_pre) VALUES (?, ?, ?, ?)",
+    [SQL_QUERY_FETCH_PRE]: "SELECT * FROM AM_Prenotazioni WHERE",
+    [SQL_QUERY_INSERT_UTENTE]: "INSERT INTO AM_Utenti(nome_utente, cognome_utente, email_utente, type_utente) VALUES (?, ?, ?, ?)",
+    [SQL_QUERY_FETCH_UTENTE]: "SELECT * FROM AM_Utenti WHERE ?",
+    [SQL_QUERY_FETCH_AULA]: "SELECT * FROM AM_Aule WHERE",
+}
+
+
+// TODO: add these env vars in my system (eddu)
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT as string),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+}).promise()
+
+export class DatabaseError extends Error {
+    errno?: number;
+    code?: string;
+    sql?: string;
+  
+    constructor(message: string, sql?: string, errno?: number, code?: string, stack?: string) {
+      super(message);
+      this.name = "DatabaseError";
+      this.sql = sql;
+      this.errno = errno;
+      this.code = code;
+      this.stack = stack;
+    }
+}
+
+export async function execQuery(query: string, values?: any[]) {
+    try {
+        let dbRes;
+        if (values) dbRes = await pool.query(query, values);
+        else dbRes = await pool.query(query);
+
+        return dbRes[0];
+    } catch (err: any) {
+        throw new DatabaseError("Error while executing the query.", err.sql, err.errno, err.code, err.stack)
+    }
+}
