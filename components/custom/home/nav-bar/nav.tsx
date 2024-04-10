d4981d435c06e45880c657fe9b24eb423f40b713
Luca <marchisioluca15@gmail.com>
2024-04-10 12:28:38 +0200

    [UPD] Updated UI. Ipad view needs fix

--- components/custom/home/nav-bar/nav.tsx
+++ components/custom/home/nav-bar/nav.tsx
@@ -1,17 +1,12 @@
 "use client"
 
-import { cn } from '@/lib/utils';
 import ThemeButton from './theme-button';
 import RepoButton from './repo-button';
 import ProfileButton from './profile-button';
 
-type NavProps = {
-  className?: string;
-}
-
-const Nav: React.FC<NavProps> = ({ className }) => {
+const Nav: React.FC = () => {
   return (
-    <nav id="nav-bar" className={cn(className, "flex flex-row space-x-2 items-center h-full w-fit p-2 rounded-secondary")}>
+    <nav id="nav-bar" className="w-fit h-[90%] flex flex-row space-x-2 items-center p-2">
       <ProfileButton className="aspect-square p-1" />
       <ThemeButton className="aspect-square p-1" />
       <RepoButton className="aspect-square p-1" />
