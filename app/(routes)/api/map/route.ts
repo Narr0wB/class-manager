4327e9b4ec438656d18753aa6ca229212d3585ca
Luca <marchisioluca15@gmail.com>
2024-04-29 12:06:03 +0200

    [UPD+REF] Updated.

--- app/(routes)/api/map/route.ts
+++ app/(routes)/api/map/route.ts
@@ -1,7 +1,7 @@
-import { TimeFrame } from "@/lib/backend/database";
-import { createSVGElement, Map, loadMap } from "@/lib/backend/map";
-import fs from "fs";
+import { parseSVG, Map, loadMap } from "@/lib/backend/map";
 import { NextRequest, NextResponse } from "next/server";
+import { FLOORS } from "../../(pages)/home/components/map-section/FloorProvider";
+import { TimeFrame } from "@/lib/backend/database";
 
 let FLOOR0: Map | undefined;
 let FLOOR1: Map | undefined;
@@ -10,51 +10,55 @@ let FLOOR2: Map | undefined;
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
 
-  // When the client requests a map, i.e. they are trying to render it, we should receive the current time frame object that 
-  // is stored in the client in order to render the appropriate colors
-  const floor = searchParams.get("floor") as string;
-  const theme = searchParams.get("theme") as string;
-  const timeframe_string = searchParams.get("timeframe") as string;
-  const user_email = searchParams.get("user") as string;
+  // When the client requests a map, i.e. they are trying to render it, we should receive
+  // the current time frame object that is stored in the client
+  // in order to render the appropriate colors
+  const floorParam = searchParams.get("floor") as string;
+  const themeParam = searchParams.get("theme") as string;
+  const timeframeParam = searchParams.get("timeframe") as string;
+  const userEmailParam = searchParams.get("userEmail") as string;
 
-  // if (!floor) return NextResponse.error();
-  // if (!theme) return NextResponse.error();
+  // if (!floorParam || !themeParam || !timeframeParam || !userEmailParam) return NextResponse.error();
 
-  let selected_floor;
+  let selectedFloor;
 
-  // Lazy loading: if it is the first time a user request the loading of the asset, then load it once and keep it saved in memory, so 
-  // that subsequent calls do not trigger a re-read of the file, which could be expensive
-  switch (Number(floor)) {
-    case 0: 
+  // Lazy loading: if it is the first time that a user requests the loading of the asset
+  // then load it once and keep it saved in memory, so that subsequent calls
+  // do not trigger a re-read of the file, which could be expensive
+  switch (Number(floorParam)) {
+    case FLOORS.FLOOR_0: {
       if (!FLOOR0) {
         FLOOR0 = loadMap("./public/pianoprimo.svg");
       }
 
-      selected_floor = FLOOR0; 
+      selectedFloor = FLOOR0;
       break;
-    case 1: 
+    }
+    case FLOORS.FLOOR_1: {
       if (!FLOOR1) {
         FLOOR1 = loadMap("./public/pianoprimo.svg");
       }
-      
-      selected_floor = FLOOR1;
+
+      selectedFloor = FLOOR1;
       break;
-    case 2:
+    }
+    case FLOORS.FLOOR_2: {
       if (!FLOOR2) {
         FLOOR2 = loadMap("./public/pianoprimo.svg");
       }
-      
-      selected_floor = FLOOR2;
+
+      selectedFloor = FLOOR2;
       break;
-    default: return NextResponse.error(); 
+    }
+    default: return NextResponse.error();
   }
 
-  // TODO: convert timeframe from string to a TimeFrame object
-  const timeframe = JSON.parse(timeframe_string);
+  const timeframe = JSON.parse(timeframeParam);
+  console.log(timeframe);
   timeframe.data = new Date(timeframe.data);
-  //console.log(timeframe, user_email);
 
-  const svgElement = await createSVGElement(selected_floor, timeframe, user_email, theme == "light");
+  const svgElement = await parseSVG(selectedFloor, timeframe, userEmailParam, themeParam == "light");
+  if (!svgElement) return NextResponse.error();
 
-  return NextResponse.json(svgElement!.innerHTML);
+  return NextResponse.json(svgElement.innerHTML);
 }
