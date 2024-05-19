dba87165b9a56a0687074ba98e56b35d6c6ceed0
Luca <marchisioluca15@gmail.com>
2024-05-19 21:28:22 +0200

    [UPD] Login page

--- app/(routes)/(pages)/login/page.tsx
+++ app/(routes)/(pages)/login/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { signIn, useSession } from 'next-auth/react';
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 import * as React from "react"
 
@@ -20,9 +20,20 @@ const Login: React.FC = () => {
   if (session.data) redirect("home");
 
   return (
-    // Just like in HomeClient (full screen height - header height)
-    <main className="w-full h-[calc(100vh-5rem)] flex justify-center items-center lg:grid lg:grid-cols-2">
-      <div className="lg:col-start-2 size-full flex justify-center items-center">
+    <main className="w-full h-screen flex justify-center items-center lg:grid lg:grid-cols-2">
+      <div
+        className="h-screen bg-cover bg-center hidden justify-center items-center lg:flex"
+        style={{ backgroundImage: "url('https://liceocuneo.it/wp-content/uploads/2022/09/Liceo-Pellico-Peano-Cuneo.jpg')" }}
+      />
+      <div className="relative size-full flex flex-col justify-center items-center">
+        <div className="absolute top-0 space-y-4 p-10 text-3xl">
+          Liceo Classico e Scientifico Statale<br />
+          <span className="text-4xl font-bold">Pellico-Peano</span><br />
+          Cuneo<br />
+          <Button variant="link" className="p-0" onClick={() => redirect("https://liceocuneo.it/")}>
+            Visita il sito
+          </Button>
+        </div>
         <Card className="w-max border-0 shadow-none">
           <CardHeader className="text-center">
             <CardTitle>Accedi</CardTitle>
